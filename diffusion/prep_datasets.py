b701118e90c0022dbef3568fb845c15a0d6be44f
Curtis Chong <curtischong5@gmail.com>
2024-04-08 11:29:29 -0400

    prep datasets so lattice is consistent

--- diffusion/prep_datasets.py
+++ diffusion/prep_datasets.py
@@ -1,6 +1,5 @@
 # This file converts the alexandria datasets to hdf5 format so it's faster to load
 # These hdf5 files are also more efficient since we drop unused columns
-from multiprocessing import Process
 from pymatgen.entries.computed_entries import ComputedStructureEntry
 import numpy as np
 import h5py
@@ -8,6 +7,9 @@ import json
 import bz2
 import pathlib
 import os
+import torch
+
+from diffusion.lattice_helpers import lattice_from_params, matrix_to_params
 
 ROOT_PATH = f"{pathlib.Path(__file__).parent.resolve()}/.."
 IN_DIR = f"{ROOT_PATH}/datasets/alexandria"
@@ -33,8 +35,16 @@ def prep_data_and_save_hdf5(filename):
             atomic_number_vector[i] = species.Z
         atomic_number_vectors.append(atomic_number_vector)
 
-        lattice_matrices[idx] = entry.structure.lattice.matrix
-        frac_coords_arrays.append(entry.structure.frac_coords)
+        original_lattice = torch.tensor(structure.lattice.matrix).unsqueeze(0)
+        new_lattice = lattice_from_params(matrix_to_params(original_lattice)).squeeze(0)
+        lattice_matrices[idx] = new_lattice.detach().cpu().numpy()
+
+        inv_lattice = torch.linalg.pinv(new_lattice)
+        cart_coords = torch.tensor(structure.cart_coords)
+        frac_coords_in_new_lattice = cart_coords @ inv_lattice
+        # I used the below code to verify that the caresian coords are the same in the new lattice
+        # cart_coords_in_new_lattice = frac_coords_in_new_lattice @ new_lattice
+        frac_coords_arrays.append(frac_coords_in_new_lattice.detach().cpu().numpy())
 
     # Save the data to an HDF5 file
     os.makedirs(OUT_DIR, exist_ok=True)
@@ -56,11 +66,12 @@ def main():
     processes = []
     for i in range(NUM_FILES):
         file_name = f"alexandria_ps_00{i}"
+        prep_data_and_save_hdf5(file_name)
 
-        # https://stackoverflow.com/questions/55529319/how-to-create-multiple-threads-dynamically-in-python
-        p = Process(target=prep_data_and_save_hdf5, args=(file_name,))
-        p.start()
-        processes.append(p)
+        # # https://stackoverflow.com/questions/55529319/how-to-create-multiple-threads-dynamically-in-python
+        # p = Process(target=prep_data_and_save_hdf5, args=(file_name,))
+        # p.start()
+        # processes.append(p)
 
     # Wait all processes to finish.
     for p in processes:
