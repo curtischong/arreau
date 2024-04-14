be40b6593e751fb99928b2cfd177a71d5a5be08e
Curtis Chong <curtischong5@gmail.com>
2024-04-13 21:49:35 -0400

    add shifting

--- exploration/view_generated_crystal.py
+++ exploration/view_generated_crystal.py
@@ -12,6 +12,10 @@ def main():
     crystal_file = f"{OUT_DIR}/crystals.h5"
     sample_result = load_sample_results_from_hdf5(crystal_file)
     lattice, frac_x, atomic_numbers = get_one_crystal(sample_result, 0)
+
+    # shift the crystal to make the atoms near the center of the unit cell
+    # frac_x = (frac_x + np.array([0.3, 0.6, 0.2])) % 1
+
     fig = plot_crystal(atomic_numbers, lattice, frac_x, show_bonds=False)
     fig.show()
 
