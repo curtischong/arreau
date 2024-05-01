c730d8b99a43490978c7961bfe8ccc0b60c8b824
Curtis Chong <curtischong5@gmail.com>
2024-05-01 14:31:52 -0400

    edge attributes now have scalar features

--- ponita/transforms/invariants.py
+++ ponita/transforms/invariants.py
@@ -2,6 +2,7 @@ import torch
 from torch_geometric.transforms import BaseTransform
 from ponita.geometry.invariants import invariant_attr_r2s1_fiber_bundle, invariant_attr_r2s1_point_cloud
 from ponita.geometry.invariants import invariant_attr_rn, invariant_attr_r3s2_fiber_bundle, invariant_attr_r3s2_point_cloud
+from ponita.utils.to_from_sphere import scalar_to_sphere
 
 
 class SEnInvariantAttributes(BaseTransform):
@@ -67,6 +68,7 @@ class SEnInvariantAttributes(BaseTransform):
                 return graph
             else:
                 if self.separable:
+                    # TODO: I'm assuming we do NOT need to add the features to the fiber_attr???
                     r3s2_attr, graph.fiber_attr = invariant_attr_r3s2_fiber_bundle(graph.pos, graph.ori_grid, graph.edge_index, graph.inter_atom_direction, separable=True)
 
                     # The other features that I'm using are only in the reals. It's similar to a norm. So I think
@@ -78,7 +80,8 @@ class SEnInvariantAttributes(BaseTransform):
                     angle_diff_1 = self.cosine_similarity(graph.inter_atom_direction, lattice_for_edge[:, 1, :])
                     angle_diff_2 = self.cosine_similarity(graph.inter_atom_direction, lattice_for_edge[:, 2, :])
                     
-                    graph.attr = [r3s2_attr, torch.stack([graph.dists, angle_diff_0, angle_diff_1, angle_diff_2], dim=-1)]
+                    graph.attr = torch.cat([r3s2_attr, scalar_to_sphere(torch.stack([graph.dists, angle_diff_0, angle_diff_1, angle_diff_2], dim=-1), graph.ori_grid)], dim=-1)
+
                 else:
                     graph.attr = invariant_attr_r3s2_fiber_bundle(graph.pos, graph.ori_grid, graph.edge_index, separable=False)
                 return graph
